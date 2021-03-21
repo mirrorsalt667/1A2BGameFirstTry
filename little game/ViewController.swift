f7c322026769a2749df4812515e7de266a281b66
Jpop <jjjjllears@gmail.com>
2021-03-21 13:04:03 +0800

    Initial Commit

---
+++ little game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  little game
+//
+//  Created by 黃肇祺 on 2021/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

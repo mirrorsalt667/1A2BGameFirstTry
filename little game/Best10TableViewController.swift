98dc4dd3ee70a4937aa481414d72a6b6b488648a
mirrorsalt667 <79618127+mirrorsalt667@users.noreply.github.com>
2021-12-21 00:02:33 +0800

    12/20 重整程式開始

--- little game/Best10TableViewController.swift
+++ little game/Best10TableViewController.swift
@@ -33,7 +33,7 @@ class Best10TableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "bestTenCell", for: indexPath) as! bestTenTableViewCell
 
         let row = indexPath.row
-        
+        cell.labelLayout()
         switch row {
         case 0:
             cell.numberLabel.text = ""
@@ -85,50 +85,5 @@ class Best10TableViewController: UITableViewController {
     func firstThreeOfNumberLabel(in label: UILabel) {
         label.font = UIFont.boldSystemFont(ofSize: 22)
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- little game/bestTenTableViewCell.swift
+++ little game/bestTenTableViewCell.swift
@@ -18,13 +18,52 @@ class bestTenTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
     }
-
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        
     }
 
+    
+    func labelLayout() {
+        let cellView = self.contentView
+        let width = self.contentView.frame.width //375
+        //6間隔
+        let widthArrays = [width/9, width/5, width/9, width/8, width/4]
+        var leftWidth = CGFloat(0)
+        var totalWidth = CGFloat(0)
+        for k in 0..<widthArrays.count {
+            totalWidth += widthArrays[k]
+        }
+        leftWidth = width - totalWidth
+        let theLeftWidth = leftWidth/6
+        numberLabel.translatesAutoresizingMaskIntoConstraints = false
+        numberLabel.widthAnchor.constraint(equalToConstant: widthArrays[0]).isActive = true
+        numberLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        numberLabel.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: theLeftWidth).isActive = true
+        numberLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        
+        secondLabel.translatesAutoresizingMaskIntoConstraints = false
+        secondLabel.widthAnchor.constraint(equalToConstant: widthArrays[1]).isActive = true
+        secondLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        secondLabel.leadingAnchor.constraint(equalTo: numberLabel.trailingAnchor, constant: theLeftWidth).isActive = true
+        secondLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        
+        guessTimesLabel.translatesAutoresizingMaskIntoConstraints = false
+        guessTimesLabel.widthAnchor.constraint(equalToConstant: widthArrays[2]).isActive = true
+        guessTimesLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        guessTimesLabel.leadingAnchor.constraint(equalTo: secondLabel.trailingAnchor, constant: theLeftWidth).isActive = true
+        guessTimesLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        answerLabel.widthAnchor.constraint(equalToConstant: widthArrays[3]).isActive = true
+        answerLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        answerLabel.leadingAnchor.constraint(equalTo: guessTimesLabel.trailingAnchor, constant: theLeftWidth).isActive = true
+        answerLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        dateLabel.widthAnchor.constraint(equalToConstant: widthArrays[4]).isActive = true
+        dateLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        dateLabel.leadingAnchor.constraint(equalTo: answerLabel.trailingAnchor, constant: theLeftWidth).isActive = true
+        dateLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+    }
 }
